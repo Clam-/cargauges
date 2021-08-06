8d1412e522d01ce98fb99f2e6cbe5541bf87c466
Lee <clam@nyanya.org>
2021-08-07 01:19:52 +1000

    Partial update

--- test-setup.js
+++ test-setup.js
@@ -1,61 +1,22 @@
-function batteryupdate(perc) {
-  document.getElementById("bat-percent").innerHTML = perc;
-}
-function rangeupdate(range) {
-  document.getElementById("range").innerHTML = range;
-}
-function accelupdate(range) {
-  document.gauges.get("accel-gauge").value = range;
-}
-function brakeupdate(range) {
-  document.gauges.get("brake-gauge").value = range;
-}
-function speedupdate(range) {
-  document.gauges.get("speed-gauge").value = range;
-}
-function steerupdate(range) {
-  document.gauges.get("steer-gauge").value = range;
-}
-function powerupdate(range) {
-  document.gauges.get("power-gauge").value = range;
-}
 
-
-// notes:
-// BDC:
-// Speed, brakeped: 22 DABD
-// Speed: "DIV" : "64.0", Brake ped ???
-// Accel: 22 DE9C  %
-//  "DATENTYP" : "unsigned int",
-//  "MUL" : "0.0625",
-"ID" : "0xDBE4",
-            "RES_TABELLE" : "RES_0xDBE4_D",
-            "INFO_EN" : "Read out wheel speed sensor",
-
-function doGauges() {
-  // document.gauges.get("speed-gauge").update({
-  //   colorNeedle: "#20105c",
-  //   colorNeedleEnd: '#432d96'
-  // });
+function doGauges(isClick) {
   // init data, and give gauge objcs, and func of batt
-  // "updatespeed": [["gauge",updatefunc,sendcode,recvprefix]]
-  init({
-    "fast": [
-      ["accel", accelupdate, "", ""]
-      ["brake", brakeupdate, ""]
-      ["speed", speedupdate, ""]
-      ["steer", steerupdate, ""]
-      ["power", powerupdate; ""]
-    ],
-    "medium": [],
-    "slow": [
-      ["battery", batteryupdate,""]
-      ["range", rangeupdate,""]
-    ]
-  })
+  // "updatespeed": [["gauge",updatefunc,[pids]]]
+  // ["speed", "gaugename", updatefunc, [pids]]
+  datastart(isClick, [
+    ["fast", "accel", accelupdate, ["12-DE9C"]],
+    ["fast", "brake", brakeupdate, ["40-DABD"]],
+    ["fast", "speed", speedupdate, ["40-DABD"]],
+    ["fast", "steer", steerupdate, ["30-DB57"]],
+    ["fast", "power", powerupdate, ["07-DD69", "07-DD68"]], // only last item will be dispatched
+    ["slow", "battery", batteryupdate, ["07-DDBC"]],
+    ["slow", "range", acupdate, ["78-D92C"]],
+    ["slow", "range", acouttempupdate, ["60-D112"]],
+    ["slow", "range", acintempupdate, ["78-D859"]],
+    ["slow", "range", acsettempupdate, ["78-D977"]],
+    ["med", "gear", gearupdate, ["63-D031"]],
+  ])
 }
 
-var script = document.querySelector('#gjs');
-script.addEventListener('load', function() {
-  doGauges();
-});
+window.addEventListener('load', () => { doGauges(false); });
+document.body.addEventListener("click", () => { doGauges(true); })
