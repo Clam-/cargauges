f619463d0a511becfb14523c61446166521e95ba
Clam- <985835+Clam-@users.noreply.github.com>
2026-05-17 18:30:15 +1000

    Removed legacy files

--- pymodules/TwFile.py
+++
@@ -1,7 +0,0 @@
-import mimetypes
-from twisted.web import static
-
-mimetypes.add_type("text/javascript", ".js", strict=True)
-mimetypes.add_type("text/html;charset=utf-8", ".html", strict=True)
-
-FILE = static.File("static")
--- static/renderPoll.js
+++
@@ -1,9 +0,0 @@
-import * as ui from './modules/updateui.js';
-import * as odb from './modules/obdpoller.js';
-
-function doGauges(isClick) {
-  odb.datastart(isClick, ui)
-}
-
-window.addEventListener('load', () => { doGauges(false); });
-document.body.addEventListener("click", () => { doGauges(true); })
